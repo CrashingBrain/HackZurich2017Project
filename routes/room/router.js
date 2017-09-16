18d58c05219229d3bcffb0193a9388437387750c
Luca Dolfi <ldolfi@gmail.com>
2017-09-17 00:06:39 +0200

    added postscount to the router

--- routes/room/router.js
+++ routes/room/router.js
@@ -53,63 +53,75 @@ router.get('/:room_id', function(req, res, next) {
 router.post('/:room_id', function(req, res, next) {
   var newPost = new Post(req.body);
   if (ObjectId.isValid(req.params.room_id)) {
-    // if true then post is a reply to another post
-    // add as reply of parent
-    if (newPost.parent) 
-    {
-      Post.findById(parent).exec(function(err, post){
-        if (err) return next(err);
-        if (!post) {
-          res.status(404).json({
-            message: "Trying to reply to a non-existent post."
+    // retrieve the room (for statistics)
+    Room.findById(req.params.room_id).exec(function(err, room){
+      if (err){
+        return next(err);
+      } else {
+        var currentRoom = room;
+
+        // if true then post is a reply to another post
+        // add as reply of parent
+        if (newPost.parent) 
+        {
+          Post.findById(parent).exec(function(err, post){
+            if (err) return next(err);
+            if (!post) {
+              res.status(404).json({
+                message: "Trying to reply to a non-existent post."
+              });
+            } else {
+              // save new post and augment coutn in room
+              newPost.save(function(err, savedChild) {
+                if (err) {
+                  res.status(400).json({
+                    message: "Error saving new post."
+                  });
+                } else {
+                  // now save child into parent
+                  post.children.push(saved._id);
+                  post.save(function(err, savedParent) {
+                    if (err) {
+                      res.status(400).json({
+                        message: "Could not save Post in parent post."
+                      });
+                    } else {
+                      room.postsCount++;
+                      room.save(function(err, savedRoom){
+                        res.status(201).json(savedChild);
+                      });
+                    }
+                  });
+                }
+              });
+            }
           });
         } else {
-          // save new post
-          newPost.save(function(err, saved) {
+          // no parent. then it's not a reply to another post
+          newPost.save(function(err, saved){
             if (err) {
               res.status(400).json({
                 message: "Error saving new post."
               });
             } else {
-              // now save child into parent
-              post.children.push(saved._id);
-              post.save(function(err, saved) {
-                if (err) {
-                  res.status(400).json({
-                    message: "Could not save Post in parent post."
-                  });
-                } else {
-                  res.status(201).json(saved);
-                }
-              });
+              // post saved in DB, now add it to the room.
+                room.posts.push(saved._id);
+                room.save(function(err, updatedRoom){
+                  if (err){
+                    res.status(400).json({
+                      message: "Could not save Post in Room. "
+                    });
+                  } else {
+                    res.status(201).json(saved);
+                  }
+                });
+              
             }
           });
         }
-      });
-    } else {
-      // no parent. then it's not a reply to another post
-      newPost.save(function(err, saved){
-        if (err) {
-          res.status(400).json({
-            message: "Error saving new post."
-          });
-        } else {
-          // post saved in DB, now add it to the room.
-          Room.findById(req.params.room_id).exec(function(err, room){
-            room.posts.push(saved._id);
-            room.save(function(err, updatedRoom){
-              if (err){
-                res.status(400).json({
-                  message: "Could not save Post in Room. "
-                });
-              } else {
-                res.status(201).json(updatedRoom);
-              }
-            });
-          });
-        }
-      });
-    }
+      }
+    });
+    
   }
 });
 
