438cc4c3c507364ce34f8b9234315a64825825db
falcon <aron.fiechter@gmail.com>
2017-09-16 02:20:42 +0200

    Add routers fetcher, models fetcher and config (mom's spaghetti)

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  /** MongoDB */
+  /* Url of the Mongodb server */
+  mongoUrl: "mongodb://localhost/",
+  /* Database name */
+  mongoDbName: "nepeta-dev",
+  /* Server URL */
+  url: "http://127.0.0.1:8191",
+
+  /** App */
+  /* Port */
+  PORT: 8191,
+}
---
+++ models/models.js
@@ -0,0 +1,14 @@
+/** @module models/models.js
+* Loads all models
+*/
+'use strict';
+
+var mongoose = require('mongoose');
+
+require('./Post');
+require('./Room');
+
+module.exports = {
+	// 'Post'         : mongoose.model('Post'), //TODO create and register model
+	// 'Room'         : mongoose.model('Room'), //TODO create and register model
+}
