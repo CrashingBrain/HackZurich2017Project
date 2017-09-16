33cff095430652f4b484713b83488d4a1263a092
falcon <aron.fiechter@gmail.com>
2017-09-16 04:29:49 +0200

    Add Post model

--- models/Post.js
+++ models/Post.js
@@ -16,16 +16,16 @@ const ObjectID = mongoose.Schema.Types.ObjectId;
 // require ('./Room'); //TODO implement Room
 
 const Post = exports.Claim = new mongoose.Schema({
-  // * username					String			  name of the posting user
-  // * message				    String			  actual post message
-  // * date              Date          date when the post was added
-  // * room              ObjectID      Room on which this message is posted
-  // * children          [ObjectID]    all replies to this Post (other Post objs)
-	},
-	{
-		versionKey: false,
-	}
-);
+  username :        { type : String, default : "anon" },
+  message :         { type : String, required : true },
+  date :            { type : Date, default : Date.now() },
+  // room :            { type : ObjectID, required : true }, //TODO implement Room
+  children :        [{ type : ObjectID, default : [] }],
+});
+
+Post.pre('save', function(next) {
+  next();
+});
 
 /* Register model for schema */
 mongoose.model('Post', Post);
