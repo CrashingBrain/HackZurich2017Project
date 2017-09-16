27dc7173cd8148d44ead1a9372976b2e729c7e98
falcon <aron.fiechter@gmail.com>
2017-09-16 15:46:02 +0200

    Start implementing room UI

--- routes/room/router.js
+++ routes/room/router.js
@@ -4,6 +4,9 @@
 /* Imports */
 const express = require('express');
 const middleware = require('../middleware');
+const mongoose = require('mongoose');
+const Room = mongoose.model('Room');
+const ObjectId = mongoose.Types.ObjectId;
 
 /* Create router */
 const router = express.Router();
@@ -17,11 +20,32 @@ router.get('/', function(req, res, next) {
 });
 
 /* Room id, return that room */
-router.get('/:roomid', function(req, res, next) {
-  if (req.accepts('text/html')) {
+router.get('/:room_id', function(req, res, next) {
+  /* If bad ID return */
+  if (!ObjectId.isValid(req.params.room_id)) {
+    //TODO correct, is fake
     res.render('room', {
-      title : "Nepeta"
+      title : "Nepeta",
+      // room : room_data
     });
+    // res.sendStatus(400);
+  }
+  /* Get room  and populate data */
+  else if (req.accepts('text/html')) {
+    Room.findById(req.params.room_id)
+    .populate('posts')
+    .exec(function(err, room_data) {
+      if (err) res.sendStatus(500);
+      else if (room_data) res.render('room', {
+        title : "Nepeta",
+        room : room_data,
+      });
+      else res.sendStatus(404);
+    });
+  }
+  /* If bad accepts return */
+  else {
+    res.sendStatus(400);
   }
 });
 
