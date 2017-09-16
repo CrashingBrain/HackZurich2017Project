bf03728cffcd4789e75742e9f6b637924d928ebd
Luca Dolfi <ldolfi@gmail.com>
2017-09-16 14:56:48 +0200

    readded areCommonEntities

--- routes/utils.js
+++ routes/utils.js
@@ -70,9 +70,20 @@ const Room = mongoose.model('Room');
 
 	}
 
-module.exports.areCommonEntities = function(item, newItem){
-
+module.exports.areCommonEntities = function(mainItem, newItem, lim){
+	APIutils.doEntitiesRequest(mainItem, function(mainEntities){
+		var mainNames = APIutils.getEntitiesNames(mainEntities);
+		APIutils.doEntitiesRequest(newItem, function(newEntities){
+			var newNames = APIutils.getEntitiesNames(newEntities);
+			if (mainEntities.filter((n) => newEntities.includes(n)).length >= lim){
+				return true;
+			} else {
+				return false;
+			}
+		});
+	});
 }
+
 
 /* Internal functions */
 
