06ab3ee1ac6a8212761b6904381f062da374b2a4
Lara <brusel@usi.ch>
2017-09-16 19:04:55 +0200

    fixed are common entities

--- routes/APIutils.js
+++ routes/APIutils.js
@@ -112,21 +112,25 @@ function demuxNewsItems(data, error) {
 
   for(let newsItem of items) {
     let existing = utils.getDuplicateNewsItem(newsItem.guid);
-    if(!existing) {
-      // TODO check common topic and put in existing room
-      // or
-      let itemUrl = 'http://rmb.reuters.com/rmd/rest/json/item?id=' + newsItem.id + '&channel=FES376&token=' + config.reutersToken;
+    let itemUrl = 'http://rmb.reuters.com/rmd/rest/json/item?id=' + newsItem.id + '&channel=FES376&token=' + config.reutersToken;
 
+    // if new article
+    if(!existing) {
       utils.doJSONRequest('GET', itemUrl, null, null, function(item, error) {
         if(error) return;
-
-        utils.openRoom(item);
+        // check common tags and either put in existing room or create new one
+        utils.demuxItem(item);
       });
+    // if existing article
     } else if(existing.version < newsItem.version) {
-      // TODO update item in room @ existing.room
-      console.log('TODO');
+      utils.doJSONRequest('GET', itemUrl, null, null, function(item, error) {
+        if(error) return;
+        // update room with latest version of this news item
+        //utils.updateNewsItem(item);
+      });
     } else {
       console.log('discarded duplicate', newsItem.guid);
+      });
     }
   }
 }
