134517c79024023c75807382c0d2383863730a8b
Luca Dolfi <ldolfi@gmail.com>
2017-09-17 06:51:35 +0200

    removed garbage

--- routes/root/router.js
+++ routes/root/router.js
@@ -18,20 +18,6 @@ router.all('/', middleware.supportedMethods('GET, OPTIONS'));
 /* Home */
 router.get('/', function(req, res, next) {
   if (req.accepts('text/html')) {
-  	// TESTing
-  	var restURL = "http://rmb.reuters.com/rmd/rest/json/item?id=tag:reuters.com,2017:newsml_LYNXNPED8E1X3:1&channel=BEQ259&token=0Uar2fCpykUYW2I2MlJIwL+aykWfs2VN81kIX5wuiTI="; //only images
-  	var restURL2 = "http://rmb.reuters.com/rmd/rest/json/item?id=tag:reuters.com,2017:newsml_KCN1BQ161:12&token=0Uar2fCpykXbX0cWkkmYwszpfU2YWqWh81kIX5wuiTI=";
-  	var entitiesURL = "http://rmb.reuters.com/rmd/rest/json/itemEntities?id=tag:reuters.com,2017:newsml_KCN1BQ161:12&minScore=0.0&token=0Uar2fCpykUMsmzhXT7Na5rCbjxeKz1/81kIX5wuiTI="
-  	utils.doJSONRequest('GET', entitiesURL, null, null, function(data){
-  		// console.log(APIutils.getEntitiesNames(data));
-  	});
-  	utils.doJSONRequest('GET', restURL2, null, null, function(news){
-  		let newurui = news.versionedguid;
-  		APIutils.doEntitiesRequest(newurui, function(ents){
-  			// console.log(ents);
-  			// console.log(APIutils.getEntitiesNames(ents));
-  		});
-  	});
     res.render('index', {
       title: "Nepeta",
     });
